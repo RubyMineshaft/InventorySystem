2dbac26591008c77a03e8d5d073de082b9d455e1
mattetress <mattetress@me.com>
2021-03-23 16:58:37 -0500

    make stage not resizable

--- src/InventorySystem/Main.java
+++ src/InventorySystem/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
         primaryStage.setTitle("Inventory Management System");
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
