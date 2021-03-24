e9358d235765ff4012d63203724f0f8cc1139208
mattetress <mattetress@me.com>
2021-03-24 00:30:24 -0500

    add javadoc comments

--- src/model/InHouse.java
+++ src/model/InHouse.java
@@ -1,15 +1,27 @@
 package model;
 
+/** The In-house part class.
+ * This class creates instances of InHouse parts which inherit from the Part class and include machine IDs.
+ */
 public class InHouse extends Part {
 
     private int machineId;
 
+    /** Constructor for the InHouse class.
+     * @param id the part ID
+     * @param name the part name
+     * @param price the part's price
+     * @param stock number of units in stock
+     * @param min minimum number of units
+     * @param max maximum number of units
+     * @param machineId the machine ID
+     */
     public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
         this.machineId = machineId;
     }
 
-    /**
+    /** Getter for the machine ID.
      * @return the machine ID
      */
     public int getMachineId() {
@@ -17,7 +29,7 @@ public class InHouse extends Part {
     }
 
 
-    /**
+    /** Setter for the machine ID.
      * @param machineId the machine ID to set
      */
     public void setMachineId(int machineId) {
--- src/model/Outsourced.java
+++ src/model/Outsourced.java
@@ -1,9 +1,22 @@
 package model;
 
+
+/** The outsourced part class.  This class creates instances of Outsource parts that inherit from the Part class and
+ * have company names associated.
+ */
 public class Outsourced extends Part{
 
     private String companyName;
 
+    /** Constructor for the Outsourced part class.
+     * @param id the part id
+     * @param name the part name
+     * @param price the part's price
+     * @param stock number of units currently in stock
+     * @param min minimum number of stock allowed
+     * @param max maximum number of stock allowed
+     * @param companyName the name of the company providing the part
+     */
     public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
         super(id, name, price, stock, min, max);
         this.companyName = companyName;
