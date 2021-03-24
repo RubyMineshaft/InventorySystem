fab498235ace321dbc6ebf043a832323b98d5a74
mattetress <mattetress@me.com>
2021-03-23 20:06:42 -0500

    create InHouse class

--- src/model/InHouse.java
+++ src/model/InHouse.java
@@ -1,2 +1,26 @@
-package model;public class InHouse {
+package model;
+
+public class InHouse extends Part {
+
+    private int machineId;
+
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
+        super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    /**
+     * @return the machine ID
+     */
+    public int getMachineId() {
+        return machineId;
+    }
+
+
+    /**
+     * @param machineId the machine ID to set
+     */
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+    }
 }
