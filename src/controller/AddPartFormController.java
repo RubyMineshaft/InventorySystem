233d9bbef8c2d5ce92453e5845e08b8ebb9e2055
mattetress <mattetress@me.com>
2021-03-24 17:52:08 -0500

    allow part form to create parts

--- src/controller/AddPartFormController.java
+++ src/controller/AddPartFormController.java
@@ -2,12 +2,25 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import model.InHouse;
+import model.Inventory;
+import model.Outsourced;
 
-public class AddPartFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddPartFormController implements Initializable {
+
+    private static int currentId;
+    private Stage stage;
+    private Parent scene;
 
     @FXML
     private RadioButton inHouseRBtn;
@@ -48,8 +61,50 @@ public class AddPartFormController {
     }
 
     @FXML
-    void onActionSave(ActionEvent event) {
+    void onActionSave(ActionEvent event) throws IOException {
+        int id = generateId();
+        String name = partNameTxt.getText();
+        int stock = Integer.parseInt(partInvTxt.getText());
+        double price = Double.parseDouble(partPriceTxt.getText());
+        int max = Integer.parseInt(partMaxTxt.getText());
+        int min = Integer.parseInt(partMinTxt.getText());
 
+        if (inHouseRBtn.isSelected()) {
+            int machineId = Integer.parseInt(machCompTxt.getText());
+            Inventory.addPart(new InHouse(id, name, price, stock, min, max, machineId));
+        } else {
+            String companyName = machCompTxt.getText();
+            Inventory.addPart(new Outsourced(id, name, price, stock, min, max, companyName));
+        }
+
+        loadMainMenu(event);
     }
 
+
+
+    private void loadMainMenu(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    private int generateId(){
+        currentId++;
+        return AddPartFormController.currentId;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        inHouseRBtn.setSelected(true);
+        partIDTxt.setText("Auto Gen - Disabled");
+    }
+
+    public void setInHouse(ActionEvent event) {
+        machCompLabel.setText("Machine ID");
+    }
+
+    public void setOutsourced(){
+        machCompLabel.setText("Company Name");
+    }
 }
