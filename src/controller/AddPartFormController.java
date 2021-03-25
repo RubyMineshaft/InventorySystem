ad327c65f9a8b31e36f7f91bdde0dc481f296790
mattetress <mattetress@me.com>
2021-03-25 17:42:54 -0500

    add validations to part forms

--- src/controller/AddPartFormController.java
+++ src/controller/AddPartFormController.java
@@ -19,6 +19,7 @@ import java.util.ResourceBundle;
 public class AddPartFormController implements Initializable {
 
     private static int currentId;
+    private String errorText = "";
 
     @FXML
     private RadioButton inHouseRBtn;
@@ -31,6 +32,9 @@ public class AddPartFormController implements Initializable {
 
     @FXML
     private Label machCompLabel;
+
+    @FXML
+    private Label errorTxt;
 
     @FXML
     private TextField partNameTxt;
@@ -53,6 +57,10 @@ public class AddPartFormController implements Initializable {
     @FXML
     private TextField partMinTxt;
 
+    private int id, stock, max, min, machineId;
+    private String name, companyName;
+    private double price;
+
     @FXML
     void onActionCancel(ActionEvent event) throws IOException {
         loadMainMenu(event);
@@ -60,25 +68,25 @@ public class AddPartFormController implements Initializable {
 
     @FXML
     void onActionSave(ActionEvent event) throws IOException {
-        int id = generateId();
-        String name = partNameTxt.getText();
-        int stock = Integer.parseInt(partInvTxt.getText());
-        double price = Double.parseDouble(partPriceTxt.getText());
-        int max = Integer.parseInt(partMaxTxt.getText());
-        int min = Integer.parseInt(partMinTxt.getText());
-
-        if (inHouseRBtn.isSelected()) {
-            int machineId = Integer.parseInt(machCompTxt.getText());
-            Inventory.addPart(new InHouse(id, name, price, stock, min, max, machineId));
-        } else {
-            String companyName = machCompTxt.getText();
-            Inventory.addPart(new Outsourced(id, name, price, stock, min, max, companyName));
+        errorText = "";
+        try {
+            if (validate()) {
+                if (inHouseRBtn.isSelected()) {
+                    Inventory.addPart(new InHouse(id, name, price, stock, min, max, machineId));
+                    loadMainMenu(event);
+                } else {
+                    Inventory.addPart(new Outsourced(id, name, price, stock, min, max, companyName));
+                    loadMainMenu(event);
+                }
+            }
         }
 
-        loadMainMenu(event);
+        catch(Exception e) {
+            errorText += "Some fields contain invalid input. \n";
+            errorTxt.setText(errorText);
+        }
+
     }
-
-
 
     private void loadMainMenu(ActionEvent event) throws IOException {
         Stage stage = (Stage)((Button)event.getSource()).getScene().getWindow();
@@ -96,6 +104,7 @@ public class AddPartFormController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         inHouseRBtn.setSelected(true);
         partIDTxt.setText("Auto Gen - Disabled");
+        id = generateId();
     }
 
     public void setInHouse(ActionEvent event) {
@@ -104,5 +113,66 @@ public class AddPartFormController implements Initializable {
 
     public void setOutsourced(){
         machCompLabel.setText("Company Name");
+    }
+
+    private boolean validate() {
+        boolean hasErrors = false;
+
+        name = partNameTxt.getText();
+        if (name == "") {
+            errorText += "Name cannot be empty. \n";
+            hasErrors = true;
+        }
+
+        try { stock = Integer.parseInt(partInvTxt.getText()); }
+        catch(NumberFormatException e) {
+            errorText += "Inv must be an integer. \n";
+            hasErrors = true;
+        }
+
+        try {price = Double.parseDouble(partPriceTxt.getText());}
+        catch(NumberFormatException e){
+            errorText += "Price must be a double. \n";
+            hasErrors = true;
+        }
+
+        try {max = Integer.parseInt(partMaxTxt.getText());}
+        catch(NumberFormatException e) {
+            errorText += "Max must be a number. \n";
+            hasErrors = true;
+        }
+
+        try { min = Integer.parseInt(partMinTxt.getText()); }
+        catch(NumberFormatException e) {
+            errorText += "Min must be a number. \n";
+            hasErrors = true;
+        }
+
+        if (min > max) {
+            errorText += "Min must be less than Max. \n";
+            hasErrors = true;
+        }
+        if (!(min < stock && stock < max)) {
+            errorText += "Inv must be between Min and Max. \n";
+            hasErrors = true;
+        }
+
+        if (inHouseRBtn.isSelected()) {
+            try { machineId = Integer.parseInt(machCompTxt.getText()); }
+            catch(NumberFormatException e) {
+                errorText += "Machine ID must be a number. \n";
+                hasErrors = true;
+            }
+        } else {
+            companyName = machCompTxt.getText();
+            if (companyName == "") {
+                errorText += "Company name cannot be blank. \n";
+                hasErrors = true;
+            }
+        }
+
+        errorTxt.setText(errorText);
+
+        return !hasErrors;
     }
 }
--- src/controller/ModifyPartFormController.java
+++ src/controller/ModifyPartFormController.java
@@ -20,6 +20,10 @@ import java.util.ResourceBundle;
 public class ModifyPartFormController implements Initializable {
 
     private int listIndex;
+    private String errorText = "";
+
+    @FXML
+    private Label errorTxt;
 
     @FXML
     private RadioButton inHouseRBtn;
@@ -54,6 +58,10 @@ public class ModifyPartFormController implements Initializable {
     @FXML
     private TextField partMinTxt;
 
+    int id, stock, max, min, machineId;
+    String name, companyName;
+    double price;
+
     @FXML
     void onActionCancel(ActionEvent event) throws IOException {
         loadMainMenu(event);
@@ -61,24 +69,23 @@ public class ModifyPartFormController implements Initializable {
 
     @FXML
     void onActionSave(ActionEvent event) throws IOException {
-        int id = Integer.parseInt(partIDTxt.getText());
-        String name = partNameTxt.getText();
-        int stock = Integer.parseInt(partInvTxt.getText());
-        double price = Double.parseDouble(partPriceTxt.getText());
-        int max = Integer.parseInt(partMaxTxt.getText());
-        int min = Integer.parseInt(partMinTxt.getText());
-
-        if (inHouseRBtn.isSelected()) {
-            int machineId = Integer.parseInt(machCompTxt.getText());
-            Inventory.updatePart(listIndex, new InHouse(id, name, price, stock, min, max, machineId));
-        } else {
-            String companyName = machCompTxt.getText();
-            Inventory.updatePart(listIndex, new Outsourced(id, name, price, stock, min, max, companyName));
+        errorText = "";
+        try {
+            if (validate()) {
+                if (inHouseRBtn.isSelected()) {
+                    Inventory.updatePart(listIndex, new InHouse(id, name, price, stock, min, max, machineId));
+                    loadMainMenu(event);
+                } else {
+                    Inventory.updatePart(listIndex, new Outsourced(id, name, price, stock, min, max, companyName));
+                    loadMainMenu(event);
+                }
+            }
         }
 
-        loadMainMenu(event);
-
-    }
+        catch(Exception e) {
+            errorText += "Some fields contain invalid input. \n";
+            errorTxt.setText(errorText);
+        }    }
 
     private void loadMainMenu(ActionEvent event) throws IOException {
         Stage stage = (Stage)((Button)event.getSource()).getScene().getWindow();
@@ -96,6 +103,7 @@ public class ModifyPartFormController implements Initializable {
 
         listIndex = Inventory.getAllParts().indexOf(part);
         partIDTxt.setText(String.valueOf(part.getId()));
+        id = part.getId();
         partNameTxt.setText(part.getName());
         partInvTxt.setText(String.valueOf(part.getStock()));
         partPriceTxt.setText(String.valueOf(part.getPrice()));
@@ -111,6 +119,67 @@ public class ModifyPartFormController implements Initializable {
             machCompTxt.setText(((Outsourced) part).getCompanyName());
             outsourcedRBtn.setSelected(true);
         }
+    }
+
+    private boolean validate(){
+        boolean hasErrors = false;
+
+        name = partNameTxt.getText();
+        if (name == "") {
+            errorText += "Name cannot be empty. \n";
+            hasErrors = true;
+        }
+
+        try { stock = Integer.parseInt(partInvTxt.getText()); }
+        catch(NumberFormatException e) {
+            errorText += "Inv must be an integer. \n";
+            hasErrors = true;
+        }
+
+        try {price = Double.parseDouble(partPriceTxt.getText());}
+        catch(NumberFormatException e){
+            errorText += "Price must be a double. \n";
+            hasErrors = true;
+        }
+
+        try {max = Integer.parseInt(partMaxTxt.getText());}
+        catch(NumberFormatException e) {
+            errorText += "Max must be a number. \n";
+            hasErrors = true;
+        }
+
+        try { min = Integer.parseInt(partMinTxt.getText()); }
+        catch(NumberFormatException e) {
+            errorText += "Min must be a number. \n";
+            hasErrors = true;
+        }
+
+        if (min > max) {
+            errorText += "Min must be less than Max. \n";
+            hasErrors = true;
+        }
+        if (!(min < stock && stock < max)) {
+            errorText += "Inv must be between Min and Max. \n";
+            hasErrors = true;
+        }
+
+        if (inHouseRBtn.isSelected()) {
+            try { machineId = Integer.parseInt(machCompTxt.getText()); }
+            catch(NumberFormatException e) {
+                errorText += "Machine ID must be a number. \n";
+                hasErrors = true;
+            }
+        } else {
+            companyName = machCompTxt.getText();
+            if (companyName == "") {
+                errorText += "Company name cannot be blank. \n";
+                hasErrors = true;
+            }
+        }
+
+        errorTxt.setText(errorText);
+
+        return !hasErrors;
     }
 
     public void setInHouse(ActionEvent event) {
